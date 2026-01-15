73e612172ba1e17296efb28409cc4ecf24289007
Nayaz Mohammed <nayazmohammed93@email.com>
2026-01-15 18:06:54 +0530

    FORM  ADDED

--- src/components/Meals/MealsItem/Mealitemform.jsx
+++ src/components/Meals/MealsItem/Mealitemform.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
-const Mealitemform = () => {
+import React from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+const MealItemForm = () => {
   return (
-    <div>Mealitemform</div>
-  )
-}
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
-export default Mealitemform
+export default MealItemForm;
